6f4f6edad21a8d6b627b08c968f8ad78862c43a5
Diego Viana <diego.p.oder@hotmail.com>
2021-01-31 00:22:55 -0300

    Update main.java

--- Trabalho-Final-sobre-Padroes/src/main/main.java
+++ Trabalho-Final-sobre-Padroes/src/main/main.java
@@ -8,6 +8,10 @@ import bridge.CamisetaLisa;
 import bridge.TamanhoCamisetaP;
 import bridge.TamanhoCamisetaM;
 import bridge.TamanhoCamisetaG;
+import chain.BancoA;
+import chain.BancoB;
+import chain.BancoChain;
+import chain.IDBancos;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,28 +27,34 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        BancoChain bancos = new BancoA();
+        bancos.setNext(new BancoB());
         BancoProxy banco = new BancoProxy("marcos", "12345");
         if (banco.getUsuario() != null) {
             System.out.println("Usuário: " + banco.getUsuario());
-            System.out.println("Lista de compras:\n");
-            
+            System.out.println("\nLista de compras:");
+
             Camiseta camiseta = new CamisetaEstampada(new TamanhoCamisetaP());
             camiseta.exibe();
             camiseta = new CamisetaEstampada(new TamanhoCamisetaM());
             camiseta.exibe();
-            
+
             camiseta = new CamisetaLisa(new TamanhoCamisetaG());
             camiseta.exibe();
-            
+
             camiseta = new CamisetaListrada(new TamanhoCamisetaM());
             camiseta.exibe();
-        
+
+            System.out.println();
+
+            bancos.efetuarPagamento(IDBancos.bancoB);
+
         } else {
             System.out.println("Usuário sem acesso");
         }
-
+        System.out.println();
     }
 
 }
