2f098152a748348dab61b005d7e6c25d3028ef0d
Diego Viana <diego.p.oder@hotmail.com>
2021-01-31 00:24:03 -0300

    Create BancoB.java

---
+++ Trabalho-Final-sobre-Padroes/src/chain/BancoB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chain;
+
+/**
+ *
+ * @author diego
+ */
+
+public class BancoB extends BancoChain {
+
+	public BancoB() {
+		super(IDBancos.bancoB);
+	}
+
+	@Override
+	protected void efetuaPagamento() {
+		System.out.println("Pagamento autorizado no banco B");
+	}
+
+}
