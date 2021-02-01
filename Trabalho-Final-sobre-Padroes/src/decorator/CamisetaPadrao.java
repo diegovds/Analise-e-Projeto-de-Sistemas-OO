16cebc5524189a25ccef698d11a5e80ae10cef7c
Diego Viana <diego.p.oder@hotmail.com>
2021-02-01 16:31:46 -0300

    Create CamisetaPadrao.java

---
+++ Trabalho-Final-sobre-Padroes/src/decorator/CamisetaPadrao.java
@@ -0,0 +1,24 @@
+package decorator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author diego
+ */
+public abstract class CamisetaPadrao {
+
+    String nome;
+    double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
