ee8b87b7b613fa2ffa3c2419dda9d460f6fd288a
Diego Viana <diego.p.oder@hotmail.com>
2021-02-01 13:10:10 -0300

    Update main.java

--- Trabalho-Final-sobre-Padroes/src/main/main.java
+++ Trabalho-Final-sobre-Padroes/src/main/main.java
@@ -1,13 +1,5 @@
 package main;
 
-import bridge.Camiseta;
-import bridge.CamisetaEstampada;
-import bridge.CamisetaListrada;
-import bridge.CamisetaLisa;
-import bridge.TamanhoCamisetaP;
-import bridge.TamanhoCamisetaM;
-import bridge.TamanhoCamisetaG;
-import chain.IDPagamento;
 import facade.Facade;
 
 /*
@@ -27,27 +19,14 @@ public class main {
     public static void main(String[] args) throws Exception {
 
         Facade f = new Facade();
-        double total = 0.0;
         
         if (f.autenticacao_usuario("marcos", "12345") != null) {
             System.out.println("Usuário: " + f.autenticacao_usuario("marcos", "12345"));
             System.out.println("\nLista de vendas:");
 
-            Camiseta camiseta = new CamisetaEstampada(new TamanhoCamisetaP());
-            total = total + f.registra_venda(camiseta);
+            System.out.println("\nTotal da venda = " + f.registra_venda() + "\n");
 
-            camiseta = new CamisetaEstampada(new TamanhoCamisetaG());
-            total = total + f.registra_venda(camiseta);
-
-            camiseta = new CamisetaLisa(new TamanhoCamisetaM());
-            total = total + f.registra_venda(camiseta);
-
-            camiseta = new CamisetaListrada(new TamanhoCamisetaP());
-            total = total + f.registra_venda(camiseta);
-
-            System.out.println("\nTotal da venda = " + total + "\n");
-
-            f.registra_pagamento(IDPagamento.dinheiro);
+            f.registra_pagamento();
 
         } else {
             System.out.println("Usuário não encontrado");
