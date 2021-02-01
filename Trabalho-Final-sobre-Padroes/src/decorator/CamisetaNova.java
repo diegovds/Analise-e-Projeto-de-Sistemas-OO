816aeacdd4f9910ac99227d328049ef1818bebdc
Diego Viana <diego.p.oder@hotmail.com>
2021-02-01 16:31:41 -0300

    Create CamisetaNova.java

---
+++ Trabalho-Final-sobre-Padroes/src/decorator/CamisetaNova.java
@@ -0,0 +1,18 @@
+package decorator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author diego
+ */
+public class CamisetaNova extends CamisetaPadrao {
+
+    public CamisetaNova() {
+        nome = "Camiseta";
+        preco = 8.0;
+    }
+}
